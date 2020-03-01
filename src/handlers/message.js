7c5a46452a40e9915eaa754710721c27dca9c597
Peter Han <peter.han.9493@gmail.com>
2020-02-29 23:43:40 -0500

    Use built in cache to validate role

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -1,20 +1,14 @@
 const { guildRoleMap, roleDuration } = require('../config');
 const isValidMessage = require('../util/isValidMessage');
 
-const activeUserCache = {};
-
 module.exports = async message => {
   if (!isValidMessage(message)) return;
 
   const roleId = guildRoleMap[message.guild.id];
   if (!(message.guild.id in guildRoleMap)) return;
 
-  if (message.author.id in activeUserCache) return;
+  if (message.member.roles.cache.has(roleId)) return;
 
-  activeUserCache[message.author.id] = true;
-  setTimeout(() => {
-    delete activeUserCache[message.author.id];
-    message.member.roles.remove(roleId);
-  }, roleDuration * 1000);
   message.member.roles.add(roleId);
+  setTimeout(() => message.member.roles.remove(roleId), roleDuration * 1000);
 };
