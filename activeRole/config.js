13af01b9b7f8cc0f1b9346204782305e4c4bcb13
Peter Han <peter.han.9493@gmail.com>
2020-03-02 21:04:14 -0500

    Convert to widget

--- activeRole/config.js
+++ activeRole/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
-  events: ['message'],
   guildRoleMap: {
     '258167954913361930': {
       roleId: '682035618536095779',
